4491bb32e7c2e61e07385c219e458702ed1b90ad
Kate <cakeflanagan@gmail.com>
2016-09-10 12:50:06 -0500

    Added random color generator to game page (pretty colors)

---
+++ Colorgame.java
@@ -0,0 +1,5 @@
+import javax.swing.JApplet;
+
+public class Colorgame extends JApplet {
+    
+}
---
+++ Colorgame/src/colorgame/Colorgame.java
@@ -0,0 +1,24 @@
+package colorgame;
+import javax.swing.JApplet;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.event.*;
+/**
+ *
+ * @author kate
+ */
+public class Colorgame extends JApplet{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JSlider a = new JSlider();
+    }
+    
+    public void paint (Graphics g) {
+        
+    }
+    
+}
