0b68f01ec26ae913ee1199c142fa4d615db12917
otolithic <cakeflanagan@gmail.com>
2021-06-15 18:13:26 -0400

    Add code for irises

--- indexscripts.js
+++ indexscripts.js
@@ -1,8 +1,8 @@
 //define the canvas and attach it
 var canvas = document.createElement("canvas")
 document.body.appendChild(canvas)
-document_width=document.body.getBoundingClientRect().width 
-document_height=document.body.getBoundingClientRect().height
+var document_width=document.body.getBoundingClientRect().width 
+var document_height=document.body.getBoundingClientRect().height
 canvas.height=Math.min(document_width, document_height)
 canvas.width=canvas.height
 
@@ -21,7 +21,7 @@ eye.src = 'assets/empty_eye.png'
 var promise = new Promise(function(resolve, reject){
     //load the image(s)
     eye.addEventListener('load', function() {
-     resolve()
+    resolve()
     })  
 })
 
@@ -29,12 +29,46 @@ var promise = new Promise(function(resolve, reject){
             THIS IS WHERE IT ALL HAPPENS
 \******************************************************/
 promise.then(function(result){
-    eye_width = canvas.height/5
-    eye_height = canvas.height/5
-    x_multipliers = [0,2.5,0.8,2,3.5]
-    y_multipliers = [0,2,0.75,3,3]
-    drawOverlay(x_multipliers,y_multipliers)
+    
+    //context.clearRect()
+    //window.requestAnimationFrame(drawIrises(eye_diam, x_multipliers,y_multipliers))
+    draw()
 })
+
+function draw(){
+    var eye_diam = canvas.height/5
+    var x_multipliers = [0,0.8,2.5,2,3.5]
+    var y_multipliers = [0,0.75,2,3,2.7]
+    var angles=[0,15,0,-15,10]
+    drawIrises(eye_diam, x_multipliers,y_multipliers)
+    drawOverlay(eye_diam,angles,x_multipliers,y_multipliers)
+}
+
+// draws the irises of the eyes in white
+function drawIrises(eye_diam, x_multipliers,y_multipliers){
+    for (idx = 0; idx < x_multipliers.length; idx++) {
+        var iris_radius = eye_diam/6
+        var x_pos = eye_diam*x_multipliers[idx]+eye_diam/2
+        var y_pos = eye_diam*y_multipliers[idx]+eye_diam/2
+        context.beginPath()
+        context.arc(x_pos, y_pos, eye_diam/6, 0, 2 * Math.PI, false)
+        context.lineWidth = 3
+        context.strokeStyle = "white"
+        context.stroke()
+        context.fillStyle = "white"
+        context.fill()
+    }
+
+}
+
+// puts the transparent eye cutouts on the canvas
+function drawOverlay(eye_diam, angles, x_multipliers, y_multipliers){
+    for (idx = 0; idx < x_multipliers.length; idx++) {
+        var pos_x = eye_diam*x_multipliers[idx]
+        var pos_y = eye_diam*y_multipliers[idx]
+        drawEye(eye, angles[idx], pos_x,pos_y, eye_diam, eye_diam)
+    }  
+}
 
 function drawEye(eye, angle, targetPointX,targetPointY,w,h)
 {
@@ -51,10 +85,4 @@ function drawEye(eye, angle, targetPointX,targetPointY,w,h)
 
 function toRadians(angle){
     return angle*Math.PI/180
-}
-
-function drawOverlay(x_multipliers, y_multipliers){
-    //drawEye(eye, angle, x_middle, y_middle)
-    for (idx = 0; idx < x_multipliers.length; idx++)
-    drawEye(eye, 0, eye_width*x_multipliers[idx],eye_height*y_multipliers[idx], eye_width, eye_height)
 }
